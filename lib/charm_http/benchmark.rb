fe4341bb86cba186cb2306e9d9334736e6e12cb0
Evan Weaver <evan@cloudbur.st>
2012-03-18 01:56:01 -0700

    Fixes.

--- lib/charm_http/benchmark.rb
+++ lib/charm_http/benchmark.rb
@@ -14,6 +14,7 @@ class CharmHttp
 
       targets.each do |path, hostname|
         (dyno_min..dyno_max).each do |dynos|
+          puts "Testing #{dynos} dynos..."
           scale(path, dynos)
 
           # Find optimal concurrency
@@ -24,14 +25,14 @@ class CharmHttp
             concurrency += step
             prev_hz = hz
             hz = parallel_test(instances, hostname, (concurrency * dynos / instances.size).to_i, 10, buckets, requests_per_connection)["hz"]
-            puts "#{concurrency} -> #{hz}hz"
+            puts "Concurrency #{concurrency}: #{hz}hz"
           end
           concurrency -= step
 
           # Measure
           results[hostname] ||= {}
           results[hostname][dynos] = parallel_test(instances, hostname, (concurrency * dynos / instances.size).to_i, 60, buckets, requests_per_connection)
-          puts "Results for #{dynos} dynos"
+          puts "Results:"
           pp results[hostname][dynos]
         end
 
@@ -44,7 +45,7 @@ class CharmHttp
 
     def self.reset(instances)
       instances.each do |instance|
-        CharmHttp.ssh(instance, "killall hstress || true", nil, true)
+        CharmHttp.ssh(instance, "killall hstress || true")
       end
     end
 
@@ -64,7 +65,7 @@ class CharmHttp
     def self.test(instance, hostname, concurrency, seconds, buckets, requests_per_connection)
       results = {}
       value = CharmHttp.ssh(instance, "hummingbird/hstress -c #{concurrency} -b #{buckets} -p 1 -r #{requests_per_connection} -i 1 #{hostname} 80", seconds)
-      values = value[/(successes.*)/m, 1].split('#')
+      values = value[/(conn_successes.*)/m, 1].split('#')
       values.map! {|v| v.split(/\s+/)}
       values.each {|v| v.reject!(&:empty?) }
       values.each {|k, v, p| results[k] = v.to_i}
